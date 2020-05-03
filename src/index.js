b7c70605656ab6e6233bfe48278435c08d4f02da
TeijiW <teijiwatanabe01@gmail.com>
2020-05-03 08:38:14 -0400

    Eslint add and partial font change

--- src/index.js
+++ src/index.js
@@ -1,31 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
+import ReactDOM from "react-dom"
 
+import Login from "./screens/login"
+import Main from "./screens/main"
+import Vendedores from "./screens/vendedores"
+import Carrinho from "./screens/carrinho"
+import ListaDesejos from "./screens/listaDesejos"
+import Produtos from "./screens/produtos"
 
-import Login from './screens/login';
-import Main from './screens/main';
-import Vendedores from './screens/vendedores';
-import Carrinho from './screens/carrinho';
-import ListaDesejos from './screens/listaDesejos';
-import Produtos from './screens/produtos';
+import * as serviceWorker from "./serviceWorker"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
 
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter, Switch, Route } from 'react-router-dom' 
+const fontTheme = createMuiTheme({
+    typography: {
+        fontFamily: '"PT Sans", sans-serif',
+    },
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-        <Switch>
-            <Route path="/" exact={true} component={Login} />
-            <Route path="/main" exact={true} component={Main} />
-            <Route path="/vendedores" exact={true} component={Vendedores} />
-            <Route path="/carrinho" exact={true} component={Carrinho} />
-            <Route path="/listaDesejos" exact={true} component={ListaDesejos} />
-            <Route path="/produtos" exact={true} component={Produtos} />
-        </Switch>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <MuiThemeProvider theme={fontTheme}>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact={true} component={Login} />
+                    <Route path="/main" exact={true} component={Main} />
+                    <Route
+                        path="/vendedores"
+                        exact={true}
+                        component={Vendedores}
+                    />
+                    <Route path="/carrinho" exact={true} component={Carrinho} />
+                    <Route
+                        path="/listaDesejos"
+                        exact={true}
+                        component={ListaDesejos}
+                    />
+                    <Route path="/produtos" exact={true} component={Produtos} />
+                </Switch>
+            </BrowserRouter>
+        </MuiThemeProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
+)
 
-serviceWorker.unregister();
+serviceWorker.unregister()
